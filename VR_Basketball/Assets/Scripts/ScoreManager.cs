969dfef3e8d7a1385f7a78f50c3ff3a1e406bceb
hsiehjohnny <32057008+hsiehjohnny@users.noreply.github.com>
2020-12-22 12:00:01 +0800

    123

---
+++ VR_Basketball/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text scoretext;
+    [Header("分數")]
+    public int score;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
