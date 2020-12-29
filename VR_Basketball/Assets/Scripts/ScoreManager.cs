5e9bfa81a389266543dcb644a02791d9742289fd
hsiehjohnny <32057008+hsiehjohnny@users.noreply.github.com>
2020-12-29 11:45:20 +0800

    finish?

--- VR_Basketball/Assets/Scripts/ScoreManager.cs
+++ VR_Basketball/Assets/Scripts/ScoreManager.cs
@@ -9,10 +9,41 @@ public class ScoreManager : MonoBehaviour
     public Text scoretext;
     [Header("分數")]
     public int score;
+    [Header("投進所得分數")]
+    public int scoreIn = 2;
 
+    public  AudioClip    soundin ;
+
+    private  AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        print(other.name);
+        if (other.tag == "籃球")
+        {
+            Addscore();
+        }
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 3;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 2;
+        }
+    }
+
+    private void Addscore() 
+    {
+        score += scoreIn;
+        scoretext.text = "分數:" + score;
+        aud.PlayOneShot(soundin, Random.Range(0.5f, 2f));
     }
 }
